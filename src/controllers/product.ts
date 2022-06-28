3b6a012e03ef86f7371fddaf901f6a3407401073
jthebat <taein007@gmail.com>
2022-06-28 10:17:27 +0900

    [TAEIN}[FIX] remove mysql codes

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,81 +1,43 @@
 import { NextFunction, Request, Response } from 'express';
-//import logging from '../config/logging';
-import { Connect, Query } from '../config/mysql';
+import mongoose from 'mongoose';
+import Product from '../models/product';
 
-//const NAMESPACE = 'Books';
-
-const createProduct = async (req: Request, res: Response, next: NextFunction) => {
-    //logging.info(NAMESPACE, 'Inserting books');
-
+const createProduct = (req: Request, res: Response, next: NextFunction) => {
     let { country, make, price } = req.body;
 
-    let query = `INSERT INTO products (country, make, price) VALUES ("${country}", "${make}, "${price}")`;
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        country,
+        make,
+        price
+    });
 
-    Connect()
-        .then((connection) => {
-            Query(connection, query)
-                .then((result) => {
-                   // logging.info(NAMESPACE, 'Book created: ', result);
-
-                    return res.status(200).json({
-                        result
-                    });
-                })
-                .catch((error) => {
-                 //   logging.error(NAMESPACE, error.message, error);
-
-                    return res.status(200).json({
-                        message: error.message,
-                        error
-                    });
-                })
-                .finally(() => {
-                //    logging.info(NAMESPACE, 'Closing connection.');
-                    connection.end();
-                });
+    return product
+        .save()
+        .then((result) => {
+            return res.status(201).json({
+                book: result
+            });
         })
         .catch((error) => {
-         //   logging.error(NAMESPACE, error.message, error);
-
-            return res.status(200).json({
+            return res.status(500).json({
                 message: error.message,
                 error
             });
         });
 };
 
-const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
-  //  logging.info(NAMESPACE, 'Getting all books.');
-
-    let query = 'SELECT * FROM products';
-
-    Connect()
-        .then((connection) => {
-            Query(connection, query)
-                .then((results) => {
-       //             logging.info(NAMESPACE, 'Retrieved books: ', results);
-
-                    return res.status(200).json({
-                        results
-                    });
-                })
-                .catch((error) => {
-           //         logging.error(NAMESPACE, error.message, error);
-
-                    return res.status(200).json({
-                        message: error.message,
-                        error
-                    });
-                })
-                .finally(() => {
-                //    logging.info(NAMESPACE, 'Closing connection.');
-                    connection.end();
-                });
+const getAllProducts = (req: Request, res: Response, next: NextFunction) => {
+    Product.find()
+        .exec()
+        .then((products) => {
+            return res.status(200).json({
+                products: products,
+                count: products.length
+            });
         })
         .catch((error) => {
-         //   logging.error(NAMESPACE, error.message, error);
-
-            return res.status(200).json({
+            return res.status(500).json({
                 message: error.message,
                 error
             });
