e0378bed4319d37c93bb2ea7082007b12b03efed
jthebat <taein007@gmail.com>
2022-06-30 14:44:53 +0900

    [TaeIn]<feat> Add drinks category Model

---
+++ src/models/category.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { ICategory } from "../interfaces/category";
+
+const categorySchema: Schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+export default mongoose.model<ICategory & Document>(
+  "categories",
+  categorySchema
+);
