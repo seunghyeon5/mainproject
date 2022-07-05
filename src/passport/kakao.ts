124b37fd358b7095eb3b6dcf5376bde15d76f568
seunghyeon55 <ilralz25@naver.com>
2022-07-06 03:37:22 +0900

    [Hong]<fix>Fix Kakaopassport

--- src/passport/kakao.ts
+++ src/passport/kakao.ts
@@ -1,5 +1,4 @@
 import KakaoRouter from "passport";
-import user from "../models/user";
 const KakaoStrategy = require("passport-kakao").Strategy;
 import User from "../models/user";
 // import jwt from "jsonwebtoken";
@@ -12,18 +11,23 @@ const kakaoPassport = () => {
                 callbackURL: "http://localhost:8080/api/user/kakao/callback" // 카카오 로그인 Redirect URI 경로
             },
             async (accessToken: any, refreshToken: any, profile: any, done: any) => {
+                let email: string;
+                email = profile._json.kakao_account_email;
+                console.log(email);
+                console.log(accessToken);
                 try {
                     const existUser = await User.findOne({
                         // 카카오 플랫폼에서 로그인 했고 이메일이 일치하는경우
-                        email: profile._json.kakao_account_email
+                        email: email
                     });
+                    // console.log(email);
                     console.log(existUser);
                     // 이미 가입된 카카오 프로필이면 성공
                     if (existUser) {
                         done(null, existUser); // 로그인 인증 완료
                     } else {
                         const newUser = await User.create({
-                            email: profile._json.kakao_account_email,
+                            email: email,
                             nickname: profile.username,
                             password: "111"
                         });
