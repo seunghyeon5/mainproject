44b5edce4af0938e118f95cbc6b8baa55216735e
seunghyeon55 <ilralz25@naver.com>
2022-07-06 12:05:35 +0900

    [Hong]<docs> 비밀번호 해싱부분 삭제

--- src/models/user.ts
+++ src/models/user.ts
@@ -19,27 +19,6 @@ const UserSchema: Schema = new Schema({
     }
 });
 
-UserSchema.pre("save", function (next) {
-    const user = this;
-
-    // user가 password를 바꿀때만 hashing 비밀번호 변경은 구현 못함 ㅠㅠ
-    if (user.isModified("password")) {
-        bcrypt.genSalt(saltRounds, function (err, salt) {
-            if (err) {
-                return next(err);
-            }
-
-            bcrypt.hash(user.password, salt, function (err, hash) {
-                if (err) {
-                    return next(err);
-                }
-                user.password = hash;
-                next();
-            });
-        });
-    }
-});
-
 UserSchema.virtual("userId").get(function () {
     return this._id.toHexString();
 });
