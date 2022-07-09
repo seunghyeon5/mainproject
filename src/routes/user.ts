71a93e831be42b118edef176a3c76a9cad2f98af
seunghyeon55 <ilralz25@naver.com>
2022-07-09 20:41:24 +0900

    [Hong}<feat>Add changenickname changepassword routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -10,6 +10,8 @@ userRouter.post("/signup", user_validation.user_signUp, userController.signup);
 userRouter.post("/login", userController.login);
 userRouter.get("/me", authMiddleware, userController.checkuser);
 userRouter.delete("/", authMiddleware, userController.withdrawal);
+userRouter.put("/changenick", user_validation.user_changenickname, authMiddleware, userController.changeNickname);
+userRouter.put("/changepassword", user_validation.user_changepassword, authMiddleware, userController.changePassword);
 
 //마이페이지
 // userRouter.get("/mypage", authMiddleware, userController.getmypage);
