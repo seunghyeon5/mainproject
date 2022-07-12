671f31b3d2706e9cf558eae4cfddbdaafe4e2617
jthebat <taein007@gmail.com>
2022-07-12 19:29:26 +0900

    [TaeIn] Add fields to drink initData

--- src/initData/drinks.ts
+++ src/initData/drinks.ts
@@ -32,6 +32,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트",
       title_eng: "Absolut Vodka",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Original",
       alc: 40,
       country: "스웨덴",
@@ -42,6 +43,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 앨릭스",
       title_eng: "Absolut Elyx",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Elyx",
       alc: 42.3,
       country: "스웨덴",
@@ -52,6 +54,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 애플",
       title_eng: "Absolut Äpple",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Apple",
       alc: 40,
       country: "스웨덴",
@@ -62,6 +65,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 채리",
       title_eng: "Absolut Cherrys",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Cherry",
       alc: 40,
       country: "스웨덴",
@@ -72,6 +76,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 시트론",
       title_eng: "Absolut Citron",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Citron",
       alc: 40,
       country: "스웨덴",
@@ -82,6 +87,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 블루베리 아사이",
       title_eng: "Absolut Berri Açai",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Berri Açai",
       alc: 40,
       country: "스웨덴",
@@ -92,6 +98,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 엑스트랙",
       title_eng: "Absolut Extrakt",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Extrakt",
       alc: 35,
       country: "스웨덴",
@@ -102,6 +109,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 라임",
       title_eng: "Absolut Lime",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Lime",
       alc: 40,
       country: "스웨덴",
@@ -112,6 +120,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 자몽",
       title_eng: "Absolut Grapefruit",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Grapefruit",
       alc: 40,
       country: "스웨덴",
@@ -122,6 +131,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 배",
       title_eng: "Absolut Pears",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pear",
       alc: 38,
       country: "스웨덴",
@@ -132,6 +142,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 복숭아",
       title_eng: "Absolut Apeach",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Apeach",
       alc: 38,
       country: "스웨덴",
@@ -142,6 +153,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 바닐라",
       title_eng: "Absolut Vanilia",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Vanilia",
       alc: 38,
       country: "스웨덴",
@@ -152,6 +164,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 바닐라",
       title_eng: "Absolut Vanilia",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Vanilia",
       alc: 38,
       country: "스웨덴",
@@ -162,6 +175,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 스트로베리",
       title_eng: "Absolut Juice Strawberry",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Juice Strawberry",
       alc: 35,
       country: "스웨덴",
@@ -172,6 +186,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "앱솔루트 라즈베리",
       title_eng: "Absolut Raspberri",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Raspberri",
       alc: 38,
       country: "스웨덴",
@@ -182,6 +197,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "시락",
       title_eng: "Ciroc",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 40,
       country: "프랑스",
@@ -192,6 +208,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "시락 애플",
       title_eng: "Ciroc Apple",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Apple",
       alc: 40,
       country: "프랑스",
@@ -202,6 +219,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "시락 코코넛",
       title_eng: "Ciroc Coconut",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Coconut",
       alc: 35,
       country: "프랑스",
@@ -212,6 +230,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "시락 망고",
       title_eng: "Ciroc Mango",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Mango",
       alc: 35,
       country: "프랑스",
@@ -222,6 +241,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "시락 피치",
       title_eng: "Ciroc Peach",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Peach",
       alc: 35,
       country: "프랑스",
@@ -232,6 +252,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "시락 워터멜론",
       title_eng: "Ciroc Watermelon",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Watermelon",
       alc: 35,
       country: "프랑스",
@@ -242,6 +263,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "그레이구스",
       title_eng: "Grey Goose",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 40,
       country: "프랑스",
@@ -252,6 +274,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "그레이구스 오랜지",
       title_eng: "Grey Goose L'Orange",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "L'Orange",
       alc: 40,
       country: "프랑스",
@@ -262,6 +285,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "그레이구스 시트론",
       title_eng: "Grey Goose Le Citron",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Le Citron",
       alc: 40,
       country: "프랑스",
@@ -272,6 +296,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "그레이구스 페어",
       title_eng: "Grey Goose La Poire",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "La Poire",
       alc: 40,
       country: "프랑스",
@@ -282,6 +307,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "벨베디어",
       title_eng: "Belvedere",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 40,
       country: "폴란드",
@@ -292,6 +318,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "탱커레이",
       title_eng: "Tanqueray",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 40,
       country: "영국",
@@ -302,6 +329,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "비피터",
       title_eng: "Beefeater",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 40,
       country: "영국",
@@ -312,6 +340,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "봄베이 사파이어",
       title_eng: "Bombay Sapphire",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 40,
       country: "영국",
@@ -322,6 +351,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "핸드릭스",
       title_eng: "Hendrick's",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Cucumber",
       alc: 44,
       country: "스코틀랜드",
@@ -332,6 +362,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "바카디 골드",
       title_eng: "Bacardi Gold",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 40,
       country: "캐나다",
@@ -342,6 +373,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "바카디 슈페리어 화이트",
       title_eng: "Bacardi Superior White",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 40,
       country: "푸에르토 리코",
@@ -352,6 +384,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "캡틴 모건",
       title_eng: "Captain Morgan Original",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Pure",
       alc: 35,
       country: "푸에르토 리코",
@@ -362,6 +395,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "말리부 코코넛",
       title_eng: "Malibu Coconut",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "Coconut",
       alc: 21,
       country: "캐나다",
@@ -372,6 +406,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "1800 아네호",
       title_eng: "1800 Anejo",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 40,
       country: "멕시코",
@@ -382,6 +417,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "1800 블랑코",
       title_eng: "1800 Blanco",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 40,
       country: "멕시코",
@@ -392,6 +428,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "1800 레포사도",
       title_eng: "1800 Reposado",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 40,
       country: "멕시코",
@@ -402,6 +439,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "호세 쿠엘보",
       title_eng: "Jose Cuervo Especial Gold",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 40,
       country: "멕시코",
@@ -412,6 +450,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "패트론 아네호",
       title_eng: "Patron Anejo",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 40,
       country: "멕시코",
@@ -422,6 +461,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "패트론 레포사도",
       title_eng: "Patron Reposado",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 40,
       country: "멕시코",
@@ -432,6 +472,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "패트론 실버",
       title_eng: "Patron Silver",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 40,
       country: "멕시코",
@@ -442,6 +483,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "패트론 카페",
       title_eng: "Patron XO Cafe",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 35,
       country: "멕시코",
@@ -452,6 +494,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "힙노틱",
       title_eng: "Hpnotiq",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 17,
       country: "프랑스",
@@ -462,6 +505,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "아구아 볼리비아",
       title_eng: "Agwa De Boliva",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 30,
       country: "네덜란드",
@@ -472,6 +516,7 @@ initDrinksRouter.post("/", async (req, res) => {
     {
       title_kor: "예거마이스터",
       title_eng: "jagermeister",
+      short_description: "이후에 작성될 예정입니다.",
       flavour: "pure",
       alc: 35,
       country: "독일",
