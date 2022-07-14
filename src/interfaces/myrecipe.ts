2f4f973b9ab287c959851eeb19dbf842abba1a0d
jthebat <taein007@gmail.com>
2022-07-15 00:27:51 +0900

    [TaeIn]<feat> Add fields to myrecipe Interface

--- src/interfaces/myrecipe.ts
+++ src/interfaces/myrecipe.ts
@@ -4,6 +4,9 @@ export interface IMyrecipe {
     _id?: Types.ObjectId;
     title: string;
     image: string;
+    key: string;
     ingredients: Array<string>;
     brief_description: string;
+    userId: string;
+    nickname: string;
 }
