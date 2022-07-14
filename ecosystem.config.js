bad0ec1fbd8cd4d08f3fe1f452bc83d5ced5cf4c
seunghyeon55 <ilralz25@naver.com>
2022-07-14 20:02:02 +0900

    [Hong]<docs>code refactoring

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "btneder",
+            name: "btender",
             script: "build/app.js",
             watch: true,
             env: {
