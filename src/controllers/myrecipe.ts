821ec33ec31c14924f278bc4e551f43d42d45eda
jthebat <taein007@gmail.com>
2022-07-15 00:36:04 +0900

    [TaeIn]<feat> modify postrecipe, deleterecipe in Controller

--- src/controllers/myrecipe.ts
+++ src/controllers/myrecipe.ts
@@ -2,30 +2,44 @@ import { Request, Response, NextFunction } from "express";
 import MyRecipe from "../models/myrecipe";
 import User from "../models/user";
 import { IMyrecipe } from "../interfaces/myrecipe";
+import config from "../config/config";
+import AWS from "aws-sdk";
+import { IUser } from "../interfaces/user";
+
+AWS.config.update({
+    accessKeyId: config.aws.access_key_id,
+    secretAccessKey: config.aws.access_secret,
+    region: config.aws.region,
+  });
+const s3 = new AWS.S3();
 
 //레시피 작성
 const postrecipe = async (req: Request, res: Response) => {
-    const { title, image, ingredients, brief_description } = req.body;
-    const userId = res.locals.user._id;
-    const nickname = res.locals.user.nickname;
     try {
-        if (!userId) {
-            return res.json({ result: false, msg: "1" });
-        }
+    const { title, ingredients, brief_description } = req.body;
+    const { userId, nickname }= res.locals.user;
+    //console.log("loc",(req.file as Express.MulterS3.File).location);
+    //console.log("key",(req.file as Express.MulterS3.File).key);
+    //console.log(JSON.stringify(req.body));       
+    //console.log(typeof(ingredients)); object      
+        
         await MyRecipe.create({
             title,
-            image,
+            image: (req.file as Express.MulterS3.File).location,
+            key: (req.file as Express.MulterS3.File).key, 
             ingredients,
             brief_description,
             nickname,
             userId
         });
+        
         const user = await User.findById({ _id: userId });
         if (!user) {
             res.json({ result: false, msg: "2" });
             return;
         }
         let num: number = user.createdposts;
+
         await User.findOneAndUpdate({ _id: userId }, { $set: { createdposts: ++num } });
         return res.json({ result: true });
     } catch (err) {
@@ -77,29 +91,43 @@ const getAllmyrecipe = async (req: Request, res: Response) => {
 
 //내가 쓴 레시피 삭제
 const deleterecipe = async (req: Request, res: Response) => {
-    const userId = String(res.locals.user._id);
+  try {    
+    const { userId } = res.locals.user;
     const { myrecipeId } = req.params;
-    const existsRecipe: any = await MyRecipe.findById(myrecipeId);
-    const user: any = await User.findById({ _id: userId });
-    // console.log(userId);
-
-    try {
-        if (!user) {
-            return res.json({ result: false, msg: "1" });
-        }
-        if (existsRecipe.userId !== userId) {
-            res.json({ result: false, message: "2" });
-            return;
-        } else {
-            await MyRecipe.findByIdAndDelete(myrecipeId);
-            let num: number = user.createdposts;
-            await User.findOneAndUpdate({ _id: userId }, { $set: { createdposts: --num } });
-            return res.json({ result: true });
-        }
-    } catch (err) {
-        res.json({ result: false });
-        console.log(err);
+    const existsRecipe: IMyrecipe | null = await MyRecipe.findById(myrecipeId);
+    const user: IUser | null = await User.findById({ _id: userId });
+       
+    s3.deleteObject(
+      {
+        Bucket: String(config.aws.bucket),
+        Key: String(existsRecipe?.key),
+      },
+      (err, data) => {
+        if (err) {
+          throw err;
+        }    
+      }
+    );
+    
+    if (!user) {
+      return res.json({ result: false, msg: "1" });
     }
+    if (existsRecipe?.userId !== userId) {
+      return res.json({ result: false, message: "2" });
+    } 
+    else{
+      await MyRecipe.findByIdAndDelete(myrecipeId);
+      let num: number = user.createdposts;
+      await User.findOneAndUpdate(
+        { _id: userId },
+        { $set: { createdposts: --num } }
+      );
+      return res.json({ result: true });
+    }
+  } catch (err) {
+    res.json({ result: false });
+    console.log(err);
+  }
 };
 
 //내가 쓴 레시피 수정
