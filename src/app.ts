27b62298b9b01ae948c625e75066a5652f382842
jthebat <taein007@gmail.com>
2022-07-18 01:12:44 +0900

    [TaeIn]<feat> cors test

--- src/app.ts
+++ src/app.ts
@@ -55,9 +55,9 @@ const port = config.server.port;
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", "http://www.b-tender.com", "https://www.b-tender.com"];
+//const allowedOrigins = ["http://localhost:3000", "http://www.b-tender.com", "https://www.b-tender.com"];
 const options: cors.CorsOptions = {
-    origin: allowedOrigins,
+    origin: ["http://localhost:3000", "http://www.b-tender.com", "https://www.b-tender.com"],
     credentials: true // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
 };
 
