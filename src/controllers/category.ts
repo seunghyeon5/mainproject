66c2427164a6e270e23e35ff186eaf4475bd517b
jthebat <taein007@gmail.com>
2022-07-19 17:33:56 +0900

    [TaeIn]<refactor> refactoring category Controller

--- src/controllers/category.ts
+++ src/controllers/category.ts
@@ -1,35 +1,46 @@
 import { Request, Response } from "express";
 import Categories from "../models/category";
+import HttpStatusCode from "../common/httpStatusCode";
 
 // 전체 카테고리 조회
 const getCategories = async (req: Request, res: Response) => {
-    try {
-        const drinkCategories = await Categories.find().lean();
+  try {
 
-        res.json({ message: "success", drinkCategories });
-    } catch (error) {
-        res.status(400).send({ message: "fail", error });
-    }
+    const drinkCategories = await Categories.find().exec();
+    return res.json({ result: true, message: "success", drinkCategories });
+
+  } catch (error) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send({ result: false, message: "잘못된 요청", error });
+  }
 };
 
 // 특정 카테고리 가져오기
 const getCategory = async (req: Request, res: Response) => {
-    try {
-        const { categoryId } = req.params;
-        //console.log(categoryId);
-        const drinkCategory = await Categories.findById(categoryId).lean();
+  try {
 
-        if (drinkCategory) {
-            res.json({ message: "success", drinkCategory });
-        } else {
-            res.status(406).json({ message: "fail", error: "no exist category" });
-        }
-    } catch (error) {
-        res.status(400).send({ message: "fail", error });
+    const { categoryId } = req.params;
+    const drinkCategory = await Categories.findById(categoryId).exec();
+
+    if (drinkCategory) {
+
+      return res.json({ result: true, message: "success", drinkCategory });
+
+    } else {
+      
+      return res
+        .status(HttpStatusCode.NOT_FOUND)
+        .json({ result: false, message: "no exist category" });
     }
+  } catch (error) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send({ result: false, message: "잘못된 요청", error });
+  }
 };
 
 export default {
-    getCategories,
-    getCategory
+  getCategories,
+  getCategory,
 };
