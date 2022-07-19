57a512f01d70c2d518077fc760d306b756598c85
jthebat <taein007@gmail.com>
2022-07-19 23:15:03 +0900

    [TaeIn]<refactor> refactoring drink Controller

--- src/controllers/drink.ts
+++ src/controllers/drink.ts
@@ -1,52 +1,61 @@
 import { Request, Response } from "express";
-//import moment from "moment";
 import Drinks from "../models/drink";
-import { IDrink } from "../interfaces/drink";
+import HttpStatusCode from "../common/httpStatusCode";
 
 // 전체 리스트 출력하기
 const getDrinks = async (req: Request, res: Response) => {
-    try {
-        const drinks: IDrink = await Drinks.find().lean();
-
-        res.json({ message: "success", drinks });
-    } catch (error) {
-        res.status(400).send({ message: "fail", error });
-    }
+  try {
+    const drinks = await Drinks.find().exec();
+    return res.json({ result: true, message: "success", drinks });
+  } catch (error) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send({ result: false, message: "잘못된 요청", error });
+  }
 };
 
 // 주류 하나 가져오기
 const getDrink = async (req: Request, res: Response) => {
-    try {
-        const { drinkId } = req.params;
-        const drink: IDrink = await Drinks.findById(drinkId).lean();
+  try {
+    const { drinkId } = req.params;
+    const drink = await Drinks.findById(drinkId).exec();
 
-        if (drink) {
-            res.json({ message: "success", drink: [drink] });
-        } else {
-            res.status(406).send({ message: "fail", error: "no exist drink" });
-        }
-    } catch (error) {
-        res.status(400).send({ message: "fail", error });
+    if (drink) {
+      return res.json({ result: true, message: "success", drink: [drink] });
+    } else {
+      return res
+        .status(HttpStatusCode.NOT_FOUND)
+        .json({ result: false, message: "no exist drink" });
     }
+  } catch (error) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send({ result: false, message: "잘못된 요청", error });
+  }
 };
+
+//카테고리별 주류 가져오기
 const getDrinksByCategory = async (req: Request, res: Response) => {
-    try {
-        const { categoryId } = req.params;
+  try {
+    const { categoryId } = req.params;
+    const drink = await Drinks.find({ categoryId }).exec();
 
-        const drink: IDrink = await Drinks.find({ categoryId: categoryId }).lean();
-
-        if (drink) {
-            res.json({ message: "success", drink });
-        } else {
-            res.status(406).send({ message: "fail", error: "no exist drink" });
-        }
-    } catch (error) {
-        res.status(400).send({ message: "fail", error });
+    if (drink) {
+      return res.json({ result: true, message: "success", drink });
+    } else {
+      return res
+        .status(HttpStatusCode.NOT_FOUND)
+        .json({ result: false, message: "no exist drink" });
     }
+  } catch (error) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send({ result: false, message: "잘못된 요청", error });
+  }
 };
 
 export default {
-    getDrinks,
-    getDrink,
-    getDrinksByCategory
+  getDrinks,
+  getDrink,
+  getDrinksByCategory,
 };
