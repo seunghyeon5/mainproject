d247eb32bf4ccaa085bc63c9f482f02daf68f4a9
choibonggyu <105187837+choibonggyu@users.noreply.github.com>
2022-07-19 20:25:06 +0900

    [bong9] <feat> add mystore favorite controllers

--- src/controllers/favorite.ts
+++ src/controllers/favorite.ts
@@ -6,9 +6,12 @@ import { IFavorite } from "../interfaces/favorite";
 const postlike = async (req: Request, res: Response) => {
     const userId = String(res.locals.user._id);
     const nickname = String(res.locals.user.nickname);
-    const { myrecipeId } = req.params;
+    const { 
+        myrecipeId,
+        mystoreId,
+     } = req.params;
     try {
-        const existLike = await Favorite.findOne({ _id: userId, myrecipeId: myrecipeId, nickname: nickname });
+        const existLike = await Favorite.findOne({ _id: userId, myrecipeId: myrecipeId, mystoreId:mystoreId, nickname: nickname });
 
         if (existLike) {
             return res.json({ result: false, msg: "이미 좋아요를 누르셨습니다." });
@@ -16,6 +19,7 @@ const postlike = async (req: Request, res: Response) => {
             const favorite = await Favorite.create({
                 userId,
                 myrecipeId,
+                mystoreId,
                 nickname
             });
             res.json({ result: true, favorite });
@@ -28,9 +32,9 @@ const postlike = async (req: Request, res: Response) => {
 
 //마이레시피에 좋아요 누른사람 조회
 const getAlluser = async (req: Request, res: Response) => {
-    const { myrecipeId } = req.params;
+    const { myrecipeId,mystoreId } = req.params;
     try {
-        const getUser: Array<IFavorite> = await Favorite.find({ myrecipeId });
+        const getUser: Array<IFavorite> = await Favorite.find({ myrecipeId,mystoreId });
         res.json({ result: true, getUser });
     } catch (err) {
         console.log(err);
@@ -62,6 +66,7 @@ const getMyrecipe = async (req: Request, res: Response) => {
     const userId = String(res.locals.user._id);
     try {
         const getMyrecipe: Array<IFavorite> = await Favorite.find({ userId });
+       
         res.json({ result: true, getMyrecipe });
     } catch (err) {
         console.log(err);
@@ -69,4 +74,84 @@ const getMyrecipe = async (req: Request, res: Response) => {
     }
 };
 
-export default { postlike, getAlluser, deletelike, getMyrecipe };
+// Mystore
+// 마이스토어 좋아요 누르기
+const postStorelike = async (req: Request, res: Response) => {
+    const userId = String(res.locals.user._id);
+    const nickname = String(res.locals.user.nickname);
+    const { 
+        mystoreId,
+     } = req.params;
+    try {
+        const existLike = await Favorite.findOne({ _id: userId, mystoreId:mystoreId, nickname: nickname });
+
+        if (existLike) {
+            return res.json({ result: false, msg: "이미 좋아요를 누르셨습니다." });
+        } else {
+            const favorite = await Favorite.create({
+                userId,
+                mystoreId,
+                nickname
+            });
+            res.json({ result: true, favorite });
+        }
+    } catch (err) {
+        console.log(err);
+        res.json({ result: false });
+    }
+};
+
+// 마이스토어 좋아요 누른 사람 조회
+const getAllstoreuser = async (req: Request, res: Response) => {
+    const { mystoreId } = req.params;
+    try {
+        const getUser: Array<IFavorite> = await Favorite.find({ mystoreId });
+        res.json({ result: true, getUser });
+    } catch (err) {
+        console.log(err);
+        res.json({ result: false });
+    }
+};
+
+// 마이스토어 좋아요 삭제
+const deleteStorelike = async (req: Request, res: Response) => {
+    const { mystoreId } = req.params;
+    const userId = String(res.locals.user._id);
+    const nickname = String(res.locals.user.nickname);
+    try {
+        const findUser = await Favorite.findOne({ userId, mystoreId, nickname });
+        if (!findUser) {
+            return res.json({ result: false, msg: "좋아요를 누르지 않았습니다." });
+        } else {
+            await Favorite.findByIdAndDelete(findUser);
+            return res.json({ result: true });
+        }
+    } catch (err) {
+        console.log(err);
+        res.json({ result: false });
+    }
+};
+
+
+//내가 좋아요 누른 마이레시피 조회
+const getMystore = async (req: Request, res: Response) => {
+    const userId = String(res.locals.user._id);
+    try {
+        const getMystore: Array<IFavorite> = await Favorite.find({ userId });
+        res.json({ result: true, getMystore });
+    } catch (err) {
+        console.log(err);
+        res.json({ result: false });
+    }
+};
+
+export default { 
+    postlike,
+    getAlluser,
+    deletelike,
+    getMyrecipe,
+    postStorelike,
+    getMystore,
+    getAllstoreuser,
+    deleteStorelike
+ };
