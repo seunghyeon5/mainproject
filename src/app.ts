8dc91134653b42e01893e38df1073cc8a5eafaf9
jthebat <taein007@gmail.com>
2022-07-22 06:05:44 +0900

    [TaeIn]<refact> refactoring app.ts

--- src/app.ts
+++ src/app.ts
@@ -56,10 +56,16 @@ const port = config.server.port;
 
 const app = express();
 
-//const allowedOrigins = ["http://localhost:3000", "http://www.b-tender.com", "https://www.b-tender.com"];
+const allowedOrigins = [
+  config.server.local as string,
+  config.server.host1 as string,
+  config.server.host2 as string,
+  config.server.host3 as string,
+  config.server.host4 as string,
+];
 const options: cors.CorsOptions = {
-    origin: ["http://www.b-tender.com", "https://www.b-tender.com", "http://b-tender.com", "https://b-tender.com", "http://localhost:3000"],
-    credentials: true // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+  origin: allowedOrigins,
+  credentials: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
 };
 
 app.use(cors(options));
@@ -118,28 +124,6 @@ app.listen(port, (): void => {
     console.log("Server is running");
 });
 
-/*
-import express, {Application, Request, Response, NextFunction} from 'express';
 
-import { productRouter } from './routes/product'
-import { connect } from './schemas/index';
-
-const port = 8080; 
-
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// APIs
-app.use("/api/product", [productRouter]);
-app.get('/', (req, res) =>{
-  res.send('질럿질럿질럿')
-});
-
-app.listen(port, (): void => {
-  console.log("Server is running");
-});
-*/
 // 굳이 모든 타입을 명시해 줄 필요가 없다.
 // 자동으로 사용되는 인자 값들, 바로 뒤에 값이 적용되는 경우는 바로 타입이 명시되기 때문이다.
