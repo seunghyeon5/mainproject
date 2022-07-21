6adeee35fce59ec161f648d2732086c70b6bfd6d
jthebat <taein007@gmail.com>
2022-07-22 06:06:25 +0900

    [TaeIn]<refact> refactoring config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 const MONGO_HOST = process.env.MONGO_URL;
@@ -9,14 +10,16 @@ const MONGO_DB = process.env.MONGO_DB;
 const MONGO = {
     url: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority`
 };
-//const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 
-const SERVER_PORT = process.env.SERVER_PORT;
-
-const SERVER = {
-    // hostname: SERVER_HOSTNAME,
-    port: SERVER_PORT
+const SERVER = {    
+    local : process.env.LOCAL,
+    host1 : process.env.SERVER_HOST1,
+    host2 : process.env.SERVER_HOST2,
+    host3 : process.env.SERVER_HOST3,
+    host4 : process.env.SERVER_HOST4,
+    port: process.env.SERVER_PORT
 };
+
 const SOCIAL = {
     google_id: process.env.GOOGLE_ID,
     google_url: process.env.GOOGLE_URL,
@@ -25,15 +28,18 @@ const SOCIAL = {
     kakao_id: process.env.KAKAO_ID,
     kakao_url: process.env.KAKAO_URL
 }
+
 const AWS = {
     access_key_id: process.env.AWS_ACCESS_KEY_ID,
     access_secret: process.env.AWS_ACCESS_SECRET,
     region: process.env.AWS_REGION,
     bucket: process.env.AWS_BUCKET
 };
+
 const JWT = {
     secretKey: process.env.JWT_SECRET
 };
+
 const config = {
     mongo: MONGO,
     server: SERVER,
