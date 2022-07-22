33e6132352f9079b83b8e57c2fb66c95e88ab742
jthebat <taein007@gmail.com>
2022-07-23 07:39:07 +0900

    [TaeIn]<test> add Strategy option

--- src/passport/google.ts
+++ src/passport/google.ts
@@ -20,7 +20,7 @@ GoogleRouter.deserializeUser((user: any, done) => {
         clientID: config.social.google_id as string,
         clientSecret: config.social.google_secret as string,
         callbackURL: config.social.google_url as string,
-        scope:["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
+        scope:["openid","https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
       },
       async function ( accessToken:any, refreshToken:any, profile:any, done:any ) {           
         try {
