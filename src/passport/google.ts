50ed50ac2195009f022102e1d511e8629eb67c00
seunghyeon5 <105096793+seunghyeon5@users.noreply.github.com>
2022-07-23 05:46:40 +0900

    Revert "[Hong]<test>google test"

--- src/passport/google.ts
+++ src/passport/google.ts
@@ -2,8 +2,6 @@ import User from "../models/user";
 import GoogleRouter from "passport";
 import { Strategy } from "passport-google-oauth2";
 import config from "../config/config";
-import { IUser } from "../interfaces/user"
-import { Document, Types } from "mongoose"
 
 const googlePassport = () => {
    
@@ -14,10 +12,11 @@ const googlePassport = () => {
         clientSecret: config.social.google_secret as string,
         callbackURL: config.social.google_url as string,
       },
-      async function ( accessToken:any, refreshToken:any, profile:{ id: any; displayName: any; _json: { email: any; }; }, done: (arg0: unknown, arg1: (IUser & Document<any, any, any> & { _id: Types.ObjectId; }) | undefined) => void ) {           
+      async function ( accessToken:any, refreshToken:any, profile:any, done:any ) {           
         try {
-          const email: string = profile._json.email
-          const provider: string = "google";
+          const email: string = profile.email
+          const provider: string = profile.provider;
+          const locale:string = profile._json.locale
           console.log(profile)
            
 
@@ -30,13 +29,14 @@ const googlePassport = () => {
           } else {
             const newUser = await User.create({             
               email,
-              nickname: profile.displayName,
-              provider,             
+              nickname: profile._json.name,
+              provider,
+              locale              
             });
             return done(null, newUser);
           }
         } catch (error) {
-          return;
+          return done(error);
         }
       }
     )
