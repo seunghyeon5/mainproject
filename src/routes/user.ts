97fffe929c65ca4ef18913d1857284db316d7fb0
jthebat <taein007@gmail.com>
2022-07-23 07:20:22 +0900

    [TaeIn]<test> modify passport option

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -24,7 +24,7 @@ userRouter.get("/kakao/callback", userController.kakaoCallback);
 
 // 구글 로그인
 //userRouter.get( "/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-userRouter.get( "/google", passport.authenticate("google", { scope: ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"] }));
+userRouter.get( "/google", passport.authenticate("google"));
 // 구글 로그인 Callback
 userRouter.get("/google/callback", userController.googleCallback);
 
