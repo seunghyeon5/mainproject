f313401d31ac1a0ac2f997d34a3acf870b74609f
jthebat <taein007@gmail.com>
2022-07-24 04:17:21 +0900

    [TaeIn]<feat> Add a Router and modify recipeRouter URI

--- src/routes/recipe.ts
+++ src/routes/recipe.ts
@@ -7,10 +7,12 @@ const recipeRouter = express.Router();
 // 전체 리스트 출력하기
 recipeRouter.get("/list/all", authMiddleware, recipeController.getRecipes);
 // 레시피 상세정보 
-recipeRouter.get("/list/:recipeId", authMiddleware, recipeController.getRecipe);
+recipeRouter.get("/list/detail/:recipeId", authMiddleware, recipeController.getRecipe);
+// 레피시 추천 상위 5 
+recipeRouter.get("/list/recommended", authMiddleware, recipeController.getMostRecommendedRecipes);
 // 레시피 추천하기
-recipeRouter.put("/list/:recipeId/recommend", authMiddleware, recipeController.recommendRecipe);
+recipeRouter.put("/list/recommend/:recipeId", authMiddleware, recipeController.recommendRecipe);
 // 레시피 추천취소
-recipeRouter.put("/list/:recipeId/undorecommend", authMiddleware, recipeController.undoRecommend);
+recipeRouter.put("/list/undorecommend/:recipeId", authMiddleware, recipeController.undoRecommend);
 
 export { recipeRouter };
