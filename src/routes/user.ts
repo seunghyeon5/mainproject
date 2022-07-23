1119f075ef5c8a490d21b9e8e870863e4c416a49
jthebat <taein007@gmail.com>
2022-07-23 09:03:36 +0900

    [TaeIn]<test> google login test

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -24,7 +24,8 @@ userRouter.get("/kakao/callback", userController.kakaoCallback);
 
 // 구글 로그인
 //userRouter.get( "/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-userRouter.get( "/google", passport.authenticate("google",{scope:["openid","email", "profile"]}));
+userRouter.get( "/google", passport.authenticate("google",{scope:["openid","https://www.googleapis.com/auth/userinfo.profile","profile","email","https://www.googleapis.com/auth/userinfo.email"]}));
+
 // 구글 로그인 Callback
 userRouter.get("/google/callback", userController.googleCallback);
 
