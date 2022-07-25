e46e6c10027252487efa9944a63b9a9dac8a3a1f
jthebat <taein007@gmail.com>
2022-07-25 22:31:06 +0900

    [TaeIn] Add mutiimageuploader

--- src/middlewares/image-uploader.ts
+++ src/middlewares/image-uploader.ts
@@ -45,3 +45,33 @@ export const imageuploader = multer({
   },
   limits: { fileSize: 10 * 1024 * 1024 },
 });
+
+export const multiimageuploader = multer({
+  storage: multerS3({
+    s3: <any>s3,
+    bucket: <any>config.aws.bucket,
+    contentType: multerS3.AUTO_CONTENT_TYPE, //객체 url 선택시 자동다운로드를 방지해준다
+    acl: "public-read",
+    key: function (
+      req: Request,
+      file: Express.MulterS3.File,
+      cb: FileNameCallback
+    ) {
+      cb(null, `store/${Date.now()}${path.basename(file.originalname)}`);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "application/pdf"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg, .mp4 and .jpeg format allowed!")); //나중에 체크 mp4
+    }
+  },
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
