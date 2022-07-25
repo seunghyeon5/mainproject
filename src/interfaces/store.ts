254ec0600cb67e8f1c5e1002a78931e8799ea4ff
jthebat <taein007@gmail.com>
2022-07-25 22:28:36 +0900

    [TaeIn]<feat> Add a field to Interface

--- src/interfaces/store.ts
+++ src/interfaces/store.ts
@@ -3,8 +3,10 @@ import { Types } from "mongoose";
 export interface IStore {
     _id?: Types.ObjectId;
     title: string;
-    image: string;
-    key: string;
+    //image: string;    
+    //key: string;
+    images: Array<string>;
+    keys: Array<string>;
     address: string;
     review: string;
     userId: string;
