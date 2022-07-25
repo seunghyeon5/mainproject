e42f4e9a9d47325d4547a11ea1f0b34cc4296617
jthebat <taein007@gmail.com>
2022-07-25 22:32:11 +0900

    [TaeIn]<feat> modify some routers

--- src/routes/store.ts
+++ src/routes/store.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import storeContoller from "../controllers/store";
 import { authMiddleware } from "../middlewares/auth-middleware";
+import { multiimageuploader } from "../middlewares/image-uploader";
 
 const mystoreRouter = express.Router();
 
@@ -10,14 +11,14 @@ mystoreRouter.get("/post/list", storeContoller.getAllstore);
 // 스토어 상세조회
 mystoreRouter.get("/post/:mystoreId", authMiddleware, storeContoller.detailstore);
 
-// 스토어 작성
-mystoreRouter.post("/post", authMiddleware, storeContoller.poststore);
+// 스토어 이미지 작성
+mystoreRouter.post("/post/images", authMiddleware, multiimageuploader.array("images",5), storeContoller.postStore);
 
 // 내가 쓴 스토어 목록
 mystoreRouter.get("/post/getmystore", storeContoller.getAllmystore);
 
 // 스토어 삭제하기
-mystoreRouter.delete("/:mystoreId/delete", authMiddleware, storeContoller.deletestore);
+mystoreRouter.delete("/delete/:mystoreId", authMiddleware, storeContoller.deletestore);
 
 // 스토어 수정
 mystoreRouter.put("/:mystoreId/modify", authMiddleware, storeContoller.modifystore);
