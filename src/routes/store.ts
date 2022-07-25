5d00f890535a18f4f53663b9e03eeff0d8498090
jthebat <taein007@gmail.com>
2022-07-26 00:32:41 +0900

    [TaeIn]<feat> Add deletemystore ,modify getallmystores

--- src/routes/store.ts
+++ src/routes/store.ts
@@ -8,17 +8,18 @@ const mystoreRouter = express.Router();
 // 스토어 전체 조회
 mystoreRouter.get("/post/list", storeContoller.getAllstore);
 
+
+// 내가 쓴 스토어 목록
+mystoreRouter.get("/post/getallmystore", authMiddleware, storeContoller.getAllmystore);
+
 // 스토어 상세조회
 mystoreRouter.get("/post/:mystoreId", authMiddleware, storeContoller.detailstore);
 
 // 스토어 이미지 작성
 mystoreRouter.post("/post/images", authMiddleware, multiimageuploader.array("images",5), storeContoller.postStore);
 
-// 내가 쓴 스토어 목록
-mystoreRouter.get("/post/getmystore", storeContoller.getAllmystore);
-
 // 스토어 삭제하기
-//mystoreRouter.delete("/delete/:mystoreId", authMiddleware, storeContoller.deletestore);
+mystoreRouter.delete("/delete/:mystoreId", authMiddleware, storeContoller.deletestore);
 
 // 스토어 수정
 mystoreRouter.put("/:mystoreId/modify", authMiddleware, storeContoller.modifystore);
