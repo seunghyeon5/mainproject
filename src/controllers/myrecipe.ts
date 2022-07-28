b223a3dfca0e8f29318b1aa3809bea3b4a6ff331
jthebat <taein007@gmail.com>
2022-07-29 00:29:56 +0900

    [TaeIn]<feat> modify getAllMyRecipe and code refactoring

--- src/controllers/myrecipe.ts
+++ src/controllers/myrecipe.ts
@@ -6,7 +6,7 @@ import config from "../config/config";
 import AWS from "aws-sdk";
 import { IUser } from "../interfaces/user";
 import { IIngredient } from "../interfaces/ingredient"
-import ingredients from "../models/ingredient";;
+import ingredients from "../models/ingredient";
 import HttpStatusCode from "../common/httpStatusCode";
 
 AWS.config.update({
@@ -17,14 +17,10 @@ AWS.config.update({
 const s3 = new AWS.S3();
 
 //레시피 작성
-const postrecipe = async (req: Request, res: Response) => {
+const postRecipe = async (req: Request, res: Response) => {
     try {
         const { title, ingredients, brief_description, steps } = req.body;
-        const { userId, nickname } = res.locals.user;
-        //console.log("loc",(req.file as Express.MulterS3.File).location);
-        //console.log("key",(req.file as Express.MulterS3.File).key);
-        //console.log(JSON.stringify(req.body));
-        //console.log(typeof(ingredients)); object
+        const { userId, nickname } = res.locals.user;        
 
         await MyRecipe.create({
             title,
@@ -55,13 +51,12 @@ const postrecipe = async (req: Request, res: Response) => {
 };
 
 //레시피 전체목록조회
-const getAllrecipe = async (req: Request, res: Response) => {
+const getAllRecipes = async (req: Request, res: Response) => {
   try {
     const myrecipes = await MyRecipe.find().sort({ createdAt: "desc" });
     return res.json({
       result: true,
       message: "success",
-
       myrecipes: myrecipes.map((a) => ({
         image: a.image,
         title: a.title,
@@ -71,7 +66,7 @@ const getAllrecipe = async (req: Request, res: Response) => {
         userId: a.userId,
         _id: a._id, //myrecipe id
         createdAt: a.createdAt?.toLocaleDateString("ko-KR"),
-      })),
+      }))
     });
   } catch (error) {
     res
@@ -81,19 +76,19 @@ const getAllrecipe = async (req: Request, res: Response) => {
 };
 
 //레시피 상세조회
-const detailrecipe = async (req: Request, res: Response) => {
+const detailRecipe = async (req: Request, res: Response) => {
   try {
     const { myrecipeId } = req.params;
     const existsRecipe = await MyRecipe.findById(myrecipeId);
-
+    
     let ingredient = "";
     let temp: IIngredient | null;
     let ingredient_images: Array<string> = [];
 
     for (let i = 0; i < existsRecipe!.ingredients.length; i++) {
-     
-      ingredient = existsRecipe!.ingredients[i];
-      temp = await ingredients.findOne({ title: ingredient }).exec();
+        
+      ingredient = existsRecipe!.ingredients[i];      
+      temp = await ingredients.findOne({ title: ingredient }).exec();     
       ingredient_images.push(temp!.image);
     }
     if (existsRecipe) {
@@ -116,26 +111,40 @@ const detailrecipe = async (req: Request, res: Response) => {
 };
 
 //내가 쓴 레시피 조회
-const getAllmyrecipe = async (req: Request, res: Response) => {
-    try {
-        const { userId } = res.locals.user;
-        if (!userId) {
-            return res
-            .status(HttpStatusCode.NOT_FOUND)
-            .json({ result: false, message: "no exist user" });
-        }
-        const Myrecipe = await MyRecipe.find({ userId });
-        // console.log(userId);
-        return res.json({ result: true, message: "success", Myrecipe });
-    } catch (error) {
-        res
-          .status(HttpStatusCode.BAD_REQUEST)
-          .send({ result: false, message: "잘못된 요청", error });
+const getAllMyRecipe = async (req: Request, res: Response) => {
+  try {
+    const { userId } = res.locals.user;
+    if (!userId) {
+      return res
+        .status(HttpStatusCode.NOT_FOUND)
+        .json({ result: false, message: "no exist user" });
     }
+
+    const Myrecipe = await MyRecipe.find({ userId });
+
+    return res.json({
+      result: true,
+      message: "success",
+      Myrecipe: Myrecipe.map((a) => ({
+        image: a.image,
+        title: a.title,
+        brief_description: a.brief_description,
+        favorite_count: a.favorite_count,
+        nickname: a.nickname,
+        userId: a.userId,
+        _id: a._id, 
+        createdAt: a.createdAt?.toLocaleDateString("ko-KR"),
+      })),
+    });
+  } catch (error) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send({ result: false, message: "잘못된 요청", error });
+  }
 };
 
 //내가 쓴 레시피 삭제
-const deleterecipe = async (req: Request, res: Response) => {
+const deleteRecipe = async (req: Request, res: Response) => {
     try {
         const { userId } = res.locals.user;
         const { myrecipeId } = req.params;
@@ -143,15 +152,15 @@ const deleterecipe = async (req: Request, res: Response) => {
         const user: IUser | null = await User.findById({ _id: userId });
 
         s3.deleteObject(
-            {
-                Bucket: config.aws.bucket as string,
-                Key: existsRecipe?.key as string
-            },
-            (err, data) => {
-                if (err) {
-                    throw err;
-                }
+          {
+            Bucket: config.aws.bucket as string,
+            Key: existsRecipe?.key as string,
+          },
+          (err, data) => {
+            if (err) {
+              throw err;
             }
+          }
         );
 
         if (!user) {
@@ -176,4 +185,4 @@ const deleterecipe = async (req: Request, res: Response) => {
     }
 };
 
-export default { postrecipe, getAllrecipe, deleterecipe, detailrecipe, getAllmyrecipe};
+export default { postRecipe, getAllRecipes, deleteRecipe, detailRecipe, getAllMyRecipe};
--- src/routes/myrecipe.ts
+++ src/routes/myrecipe.ts
@@ -5,14 +5,14 @@ import { imageuploader } from "../middlewares/image-uploader";
 
 const myrecipeRouter = express.Router();
 //레시피 작성
-myrecipeRouter.post("/post", authMiddleware,imageuploader.single("image"), myrecipeController.postrecipe);
+myrecipeRouter.post("/post", authMiddleware,imageuploader.single("image"), myrecipeController.postRecipe);
 //레시피 전체목록 조회
-myrecipeRouter.get("/post/list", authMiddleware, myrecipeController.getAllrecipe);
+myrecipeRouter.get("/post/list", authMiddleware, myrecipeController.getAllRecipes);
 //내가 쓴 레시피 조회
-myrecipeRouter.get("/post/getmyrecipe", authMiddleware, myrecipeController.getAllmyrecipe);
+myrecipeRouter.get("/post/getmyrecipe", authMiddleware, myrecipeController.getAllMyRecipe);
 //레시피 상세조회
-myrecipeRouter.get("/post/detail/:myrecipeId", authMiddleware, myrecipeController.detailrecipe);
+myrecipeRouter.get("/post/detail/:myrecipeId", authMiddleware, myrecipeController.detailRecipe);
 //레시피 삭제
-myrecipeRouter.delete("/:myrecipeId/delete", authMiddleware, myrecipeController.deleterecipe);
+myrecipeRouter.delete("/:myrecipeId/delete", authMiddleware, myrecipeController.deleteRecipe);
 
 export { myrecipeRouter };
