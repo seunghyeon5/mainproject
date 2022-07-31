071a2ef180f096a31e2611b736f2b2234c0169e4
jthebat <taein007@gmail.com>
2022-07-31 13:46:47 +0900

    [TaeIn]<feat> modify some fields in Interface

--- src/interfaces/favorite.ts
+++ src/interfaces/favorite.ts
@@ -3,10 +3,8 @@ import { Types } from "mongoose";
 export interface IFavorite {
     _id?: Types.ObjectId;
     nickname: string;
-    myrecipeId: string;
-    MystoreId:string;
+    myfavoritesId: string; 
+    userId:string; 
     category:string;
-    Myrecipe:Array<string>;
-    Store:Array<string>;
-    userId:string;
+    myfavoritesInfo:string;   
 }
