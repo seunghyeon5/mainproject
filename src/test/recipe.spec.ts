33ccfc426ef8ac3e3299ea0e1e7a1f1db6e2bce0
seunghyeon55 <ilralz25@naver.com>
2022-08-04 17:58:56 +0900

    [Hong]<test>테스트 코드

--- src/test/recipe.spec.ts
+++ src/test/recipe.spec.ts
@@ -17,7 +17,12 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-describe("[POST] 로그인", () => {
+describe("로그인", () => {
+    test("로그인을 하지 않은 경우 401", async () => {
+        const response = await request(app).get("/api/recipe/list/detail/" + testdata.recipeId).auth("", "").send()
+
+        expect(response.statusCode).toBe(401)
+    })
     test("로그인 성공시 success", async () => {
         const response = await request(app).post("/api/user/login").send({ email: testdata.email, password: testdata.pw });
 
@@ -26,4 +31,37 @@ describe("[POST] 로그인", () => {
         expect(response.body.message).toBe("success");
         expect(response.body.token).toBe(`${token}`);
     });
+})
+describe("[POST] Recipe", () => {
+    test("레시피 추천누르기 성공할시 message:추천", async () => {
+        const response = await request(app).put("/api/recipe/list/recommend/" + testdata.recipeId).set("authorization", `Bearer ${token}`).send()
+
+        expect(response.body.message).toBe("추천")
+    })
+})
+
+describe("[GET] Recipe", () => {
+    test("레시피 상세조회", async () => {
+        const response = await request(app).get("/api/recipe/list/detail/" + testdata.recipeId).set("authorization", `Bearer ${token}`).send()
+
+        expect(response.statusCode).toBe(200)
+    })
+    test("레시피 검색", async () => {
+        const response = await request(app).get("/api/recipe/list/search/gin").set("authorization", `Bearer ${token}`).send()
+
+        expect(response.statusCode).toBe(201)
+    })
+    test("추천누른 레시피 조회", async () => {
+        const response = await request(app).get("/api/recipe/list/getrecipe").set("authorization", `Bearer ${token}`).send()
+
+        expect(response.statusCode).toBe(200)
+    })
+})
+
+describe("[DELETE] Recipe", () => {
+    test("레시피 추천취소하기", async () => {
+        const response = await request(app).put("/api/recipe/list/undorecommend/" + testdata.recipeId).set("authorization", `Bearer ${token}`).send()
+
+        expect(response.statusCode).toBe(201)
+    })
 })
--- src/test/testdata.ts
+++ src/test/testdata.ts
@@ -1,19 +1,24 @@
-const mockdata ={
+const testdata ={
     // user data
     Email: "test00@naver.com",
     Pw: "zxc123!!",
     Pwcheck: "zxc123!!",
     Nick: "lijefd12",
+    ChangeNick: "polkm12",
+    ChangePw: "zczc123!",
     wrongEmail:"eofmd@test.com",
     wrongPw:"nknk121@",
-    mockdrinkId: "62e32cef0cd90901a1b3897d",
-    mockstoreId: "62e38b2d98c2049704b5172d",
-    mockrecipeId: "62e49affaaeb4b997108b4f1",
-    mockmyrecipeId: "62e3d40298c2049704b5404d",
-    mockcategoryId: "62c3de5f57b3cc6babc431bf",
+    drinkId: "62e32cef0cd90901a1b3897d",
+    storeId: "62e38b2d98c2049704b5172d",
+    recipeId: "62e49affaaeb4b997108b4f1",
+    wrongrecipeId: "62e49affaaeb4b997108b4f7",
+    myrecipeId: "62e3d40298c2049704b5404d",
+    categoryId: "62c3de5f57b3cc6babc431bf",
     email: "test01@google.com",
     pw:"zxc123!!",
-    nick:"liejfd11"
+    nick:"liejfd11",
+    email2: "test48@test.com",
+    pw2:"zxc123!!",
 }
 
-export default mockdata;
+export default testdata;
